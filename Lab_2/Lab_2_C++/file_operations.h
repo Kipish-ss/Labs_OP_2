17a38688fedcd3a8fcb19bdc23c302d0c522d677
Kyryl Sidak <ksidak7@gmail.com>
2022-04-08 14:58:01 +0300

    Lab_2_C++ done

--- Lab_2/Lab_2_C++/file_operations.h
+++ Lab_2/Lab_2_C++/file_operations.h
@@ -1,4 +1,21 @@
 #ifndef LAB_2_C___FILE_OPERATIONS_H
 #define LAB_2_C___FILE_OPERATIONS_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
 
+struct Car {
+    string model;
+    string release_date;
+    string sell_date;
+};
+string enter_file_mode();
+void create_fist_file(string, int, string);
+void output_file(string);
+void create_list_recent_cars(string, string, int, int);
+void output_worn_cars(string);
+void output_car(Car);
+vector<string> split_date(string);
 #endif
--- Lab_2/Lab_2_C++/main.cpp
+++ Lab_2/Lab_2_C++/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
 #include "file_operations.h"
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, month, year;
+    string file_1 = "/Users/kyryl/Downloads/Labs_OP_2/Lab_2/Lab_2_C++/cars_1.txt";
+    string file_2 = "/Users/kyryl/Downloads/Labs_OP_2/Lab_2/Lab_2_C++/cars_2.txt";
+    string file_mode = enter_file_mode();
+    cout << "Enter the number of cars:" << endl;
+    cin >> n;
+    create_fist_file(file_1, n, file_mode);
+    cout << "List of cars(first file):" << endl;
+    output_file(file_1);
+    cout << "Enter the month number:" << endl;
+    cin >> month;
+    cout << "Enter the year:" << endl;
+    cin >> year;
+    create_list_recent_cars(file_1, file_2, month, year);
+    cout << "List of cars which started selling this month(second file):" << endl;
+    output_file(file_2);
+    output_worn_cars(file_1);
     return 0;
 }
