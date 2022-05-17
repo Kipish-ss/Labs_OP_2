f1dc61e4f3d7dd73399763c45f40f4f365aae7e3
Kyryl Sidak <ksidak7@gmail.com>
2022-05-17 10:55:38 +0300

    Finish lab_5_c++

---
+++ Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/Parallelogram.cpp" "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/Parallelogram.cpp.o"
+  "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/Point.cpp" "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/Point.cpp.o"
+  "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/TQuadrangle.cpp" "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/TQuadrangle.cpp.o"
+  "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/input_operations.cpp" "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/input_operations.cpp.o"
+  "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/main.cpp" "/Users/kyryl/Downloads/Labs_OP_2/Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab_5/Lab_5_C++/cmake-build-debug/CMakeFiles/Lab_5_C__.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_5_C__.dir/Parallelogram.cpp.o"
+  "CMakeFiles/Lab_5_C__.dir/Point.cpp.o"
+  "CMakeFiles/Lab_5_C__.dir/TQuadrangle.cpp.o"
+  "CMakeFiles/Lab_5_C__.dir/input_operations.cpp.o"
+  "CMakeFiles/Lab_5_C__.dir/main.cpp.o"
+  "Lab_5_C__"
+  "Lab_5_C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_5_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
